0871ff7504e2b72e07fc870aa5ee6192f96db70f
Thorsten Deinert <thorsten.deinert@udo.edu>
2019-01-24 11:38:14 +0100

    configurable osm endpoint via :gecoder, Geocoder.Worker, :endpoint

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,6 @@ config :geocoder, :worker_pool_config,
   size: 4,
   max_overflow: 2
 
-config :geocoder, :worker, provider: Geocoder.Providers.OpenStreetMaps
+config :geocoder, :worker,
+  provider: Geocoder.Providers.OpenStreetMaps,
+  endpoint: nil
